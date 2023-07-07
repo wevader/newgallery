9dde15f50bff957f392f5cf14b9b77bd672e6e55
wevader <wevader1986@gmail.com>
2023-07-07 12:26:34 -0600

    new sideproyect

---
+++ src/cargarCategorias.js
@@ -0,0 +1,24 @@
+import dataCategorias from './datos/categorias'
+console.log(dataCategorias)
+const {categorias} = dataCategorias
+const contenedorCategorias = document.getElementById('categorias')
+
+
+
+categorias.forEach((categoria) => {
+    console.log(categorias)
+    const nuevaCategoria = document.createElement('a')
+    const plantilla = `<img class="categoria__img" src="${categoria.imagenPortada} " alt="" />
+    <div class="categoria__datos">
+        <p class="categoria__nombre"> ${categoria.nombre} </p>
+        <p class="categoria__numero-fotos"> ${categoria.numeroFotos} Fotos </p>
+    </div>`
+
+    nuevaCategoria.innerHTML = plantilla
+    nuevaCategoria.classList.add('categoria')
+    nuevaCategoria.href = '#'
+    nuevaCategoria.dataset.categoria = categoria.id
+
+    contenedorCategorias.append(nuevaCategoria)
+
+})
---
+++ src/index.js
@@ -0,0 +1,1 @@
+import './cargarCategorias.js'
